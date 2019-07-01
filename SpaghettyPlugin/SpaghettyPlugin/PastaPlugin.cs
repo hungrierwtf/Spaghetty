2d5acdddd7ad2233e4c0f70c870aad929d4a5938
hungrier <hungrierwtf@gmail.com>
2019-07-01 00:45:19 -0400

    moved the stuff around

--- SpaghettyPlugin/SpaghettyPlugin/PastaPlugin.cs
+++ SpaghettyPlugin/SpaghettyPlugin/PastaPlugin.cs
@@ -16,13 +16,69 @@ namespace SpaghettyPlugin
 
 		private readonly string PLUGIN_LOWER = PLUGIN_NAME.ToLowerInvariant();
 
-		private IDictionary<string, string> spagBowl;
+		private IDictionary<string, string> _spagBowl;
+		private IPluginHost _pluginHost;
+		private ICatItemFactory _catFactory;
+		private uint? _hash;
+		private string _iconPath;
 
-		private IPluginHost pluginHost;
-		private ICatItemFactory catFactory;
-		private uint? spHash;
-		private string spIconPath;
 
+		public void init(IPluginHost pluginHost)
+		{
+			this._pluginHost = pluginHost;
+			this._catFactory = pluginHost.catItemFactory();
+
+			_spagBowl = new Dictionary<string, string>();
+
+
+
+
+			//TODO remove this
+			_spagBowl.Add("facepalm-world", "![7f9158a9-7c33-43a2-8bfe-e081afb356ae-image.png](/assets/uploads/files/1561910847061-7f9158a9-7c33-43a2-8bfe-e081afb356ae-image.png)");
+
+		}
+
+		#region info getters
+		public uint getID()
+		{
+			if (!_hash.HasValue)
+			{
+				_hash = _pluginHost.hash(PLUGIN_NAME);
+			}
+
+			return _hash.Value;
+		}
+
+		public string getName()
+		{
+			return PLUGIN_NAME;
+		}
+
+		private string getIcon()
+		{
+			if (string.IsNullOrEmpty(_iconPath))
+			{
+				var iconPath = _pluginHost.launchyPaths().getIconsPath();
+				_iconPath = System.IO.Path.Combine(iconPath, ICON_FILENAME);
+			}
+
+			return _iconPath;
+		}
+		#endregion
+
+		#region launchy show and hide triggers
+		public void launchyHide()
+		{
+			//MessageBox.Show("Launchy hide, this will also be annoying");
+		}
+
+		public void launchyShow()
+		{
+			//MessageBox.Show("launchy show, no way this will get annoying");
+		}
+		#endregion
+
+		#region dialog stuff
 		public IntPtr doDialog()
 		{
 			return IntPtr.Zero;
@@ -33,19 +89,16 @@ namespace SpaghettyPlugin
 			//TODO figure out wtf
 		}
 
+		public bool hasDialog()
+		{
+			return false;
+		}
+		#endregion
+
+		#region result management
 		public void getCatalog(List<ICatItem> catalogItems)
 		{
-			catalogItems.Add(catFactory.createCatItem(PLUGIN_NAME + SPG_EXT, PLUGIN_NAME, getID(), getIcon()));
-		}
-
-		public uint getID()
-		{
-			if (!spHash.HasValue)
-			{
-				spHash = pluginHost.hash(PLUGIN_NAME);
-			}
-
-			return spHash.Value;
+			catalogItems.Add(_catFactory.createCatItem(PLUGIN_NAME + SPG_EXT, PLUGIN_NAME, getID(), getIcon()));
 		}
 
 		public void getLabels(List<IInputData> inputDataList)
@@ -59,17 +112,12 @@ namespace SpaghettyPlugin
 			}
 		}
 
-		public string getName()
-		{
-			return PLUGIN_NAME;
-		}
-
 		public void getResults(List<IInputData> inputDataList, List<ICatItem> resultsList)
 		{
 			var sp = inputDataList[0];
 			if (!sp.hasLabel(getID())) { return; }
 
-			var keys = spagBowl.Keys;
+			var keys = _spagBowl.Keys;
 			var inputText = "";
 
 
@@ -116,8 +164,8 @@ namespace SpaghettyPlugin
 
 		private ICatItem createCat(string text, string fullPath)
 		{
-			var hash = pluginHost.hash(fullPath);
-			return catFactory.createCatItem(fullPath, text, hash, getIcon());
+			var hash = _pluginHost.hash(fullPath);
+			return _catFactory.createCatItem(fullPath, text, hash, getIcon());
 		}
 
 		private ICatItem createCat(string text)
@@ -132,38 +180,78 @@ namespace SpaghettyPlugin
 			var label = $"New pasta '{text}'";
 			return createCat(label, fullPath);
 		}
+		#endregion
 
-		private string getIcon()
+		#region launching
+		public void launchItem(List<IInputData> inputDataList, ICatItem item)
 		{
-			if (string.IsNullOrEmpty(spIconPath))
+			//so
+			// item is worthless, it's just the main Spaghetty
+			// inputDataList, and specifically .getTopResult of each input item, is what i want
+			// so possibilities:
+			// one input (just spaghetty): ? maybe nothing
+			// two inputs: show the selected one; maybe open it for editing
+			// three inputs: create/replace? maybe
+
+
+
+			if (inputDataList.Count < 2)
 			{
-				var iconPath = pluginHost.launchyPaths().getIconsPath();
-				spIconPath = System.IO.Path.Combine(iconPath, ICON_FILENAME);
+				var lp = _pluginHost.launchyPaths();
+				var sb = new StringBuilder();
+				var nl = Environment.NewLine;
+
+				sb.Append(lp.getConfigPath());
+				sb.Append(nl);
+				sb.Append(lp.getIconsPath());
+				sb.Append(nl);
+				sb.Append(lp.getLaunchyPath());
+
+				MessageBox.Show(sb.ToString());
+				return;
 			}
 
-			return spIconPath;
+			var selectedPasta = inputDataList[1].getTopResult();
+			var fp = selectedPasta.getFullPath();
+
+			string key = "nothing";
+			string text = "if you can read this, something went wrong";
+			Boolean newPasta = false;
+
+			if (fp.EndsWith(SPG_EXT))
+			{
+				key = fp.Substring(0, fp.Length - SPG_EXT.Length);
+				_spagBowl.TryGetValue(key, out text);
+			}
+			else if (fp.EndsWith(NEW_EXT))
+			{
+				key = fp.Substring(0, fp.Length - NEW_EXT.Length);
+				newPasta = true;
+				if (inputDataList.Count > 2)
+				{
+					text = inputDataList[2].getText();
+				}
+				else
+				{
+					text = "";
+				}
+			}
+
+			var pw = new DisplayPasta(key, text);
+			if (newPasta)
+			{
+				pw.SetOriginalText("");
+			}
+
+			var result = pw.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				_spagBowl[key] = pw.GetText();
+			}
 		}
+		#endregion
 
-		public bool hasDialog()
-		{
-			return false;
-		}
-
-		public void init(IPluginHost pluginHost)
-		{
-			this.pluginHost = pluginHost;
-			this.catFactory = pluginHost.catItemFactory();
-
-			spagBowl = new Dictionary<string, string>();
-
-
-
-
-			//TODO remove this
-			spagBowl.Add("facepalm-world", "![7f9158a9-7c33-43a2-8bfe-e081afb356ae-image.png](/assets/uploads/files/1561910847061-7f9158a9-7c33-43a2-8bfe-e081afb356ae-image.png)");
-
-		}
-
+		#region debug
 		private void showLaunchItemStuff(List<IInputData> inputDataList, ICatItem item)
 		{
 			var sb = new StringBuilder();
@@ -232,82 +320,6 @@ namespace SpaghettyPlugin
 
 			MessageBox.Show(sb.ToString());
 		}
-
-		public void launchItem(List<IInputData> inputDataList, ICatItem item)
-		{
-			//so
-			// item is worthless, it's just the main Spaghetty
-			// inputDataList, and specifically .getTopResult of each input item, is what i want
-			// so possibilities:
-			// one input (just spaghetty): ? maybe nothing
-			// two inputs: show the selected one; maybe open it for editing
-			// three inputs: create/replace? maybe
-
-
-
-			if (inputDataList.Count < 2)
-			{
-				var lp = pluginHost.launchyPaths();
-				var sb = new StringBuilder();
-				var nl = Environment.NewLine;
-
-				sb.Append(lp.getConfigPath());
-				sb.Append(nl);
-				sb.Append(lp.getIconsPath());
-				sb.Append(nl);
-				sb.Append(lp.getLaunchyPath());
-
-				MessageBox.Show(sb.ToString());
-				return;
-			}
-
-			var selectedPasta = inputDataList[1].getTopResult();
-			var fp = selectedPasta.getFullPath();
-
-			string key = "nothing";
-			string text = "if you can read this, something went wrong";
-			Boolean newPasta = false;
-
-			if (fp.EndsWith(SPG_EXT))
-			{
-				key = fp.Substring(0, fp.Length - SPG_EXT.Length);
-				spagBowl.TryGetValue(key, out text);
-			}
-			else if (fp.EndsWith(NEW_EXT))
-			{
-				key = fp.Substring(0, fp.Length - NEW_EXT.Length);
-				newPasta = true;
-				if (inputDataList.Count > 2)
-				{
-					text = inputDataList[2].getText();
-				}
-				else
-				{
-					text = "";
-				}
-			}
-
-			var pw = new DisplayPasta(key, text);
-			if (newPasta)
-			{
-				pw.SetOriginalText("");
-			}
-
-			var result = pw.ShowDialog();
-			if (result == DialogResult.OK)
-			{
-				spagBowl[key] = pw.GetText();
-			}
-		}
-
-		public void launchyHide()
-		{
-			//MessageBox.Show("Launchy hide, this will also be annoying");
-		}
-
-		public void launchyShow()
-		{
-			//MessageBox.Show("launchy show, no way this will get annoying");
-		}
+		#endregion
 	}
 }
